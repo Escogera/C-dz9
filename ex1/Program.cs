9bca17c91b3b3688262f5787f59f44dde9d55f4c
Vladimir <maostyle@mail.ru>
2022-08-24 15:25:25 +0300

    complete HW

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+Console.WriteLine("Введите первое число");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число, больше первого");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string NumbersRec(int M, int N)
+{
+    if (M <= N) return NumbersRec(M, N - 1) + $"{N}, ";
+    else return String.Empty;
+}
+Console.WriteLine($"{NumbersRec(M, N)}");
--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число, больше первого");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumRec(int M, int N)
+{
+    if (N == M) return M;
+    else return N + SumRec(M, N - 1);
+}
+
+Console.WriteLine($"{SumRec(M, N)}");
